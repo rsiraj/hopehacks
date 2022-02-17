e05655e1fcdaba3600604e00bdd33a5abeeea2fc
Rehan Siraj <rsiraj@r2hstudent.org>
2022-02-16 22:57:35 -0500

    updated backend

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const request = require("request");
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const bodyParser = require('body-parser')
 
-const apiKey = "76ef3b44b1364389bc480394115044cf"
+const apiKey = process.env.APIKEY
 // let dailyCals = 2300
 // let diet = 'vegetarian'
 
@@ -20,22 +21,31 @@ app.get('/', (req, res)=> {
 })
 
 app.post('/', (req, res)=> {
-    let dailyCals = req.body.calories
+    let dailyCals = req.body.dailyCals
     let diet = req.body.diet
 
     let url = `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day&targetCalories=${dailyCals}&diet=${diet}`
 
-    request (url, function(err, res, body){
+    request (url, function(err, response, body){
         if(err){
             console.log(`Error: ${err}`)
+
+            res.render('index', {dailyCals:null, error: err})
         }else {
             // console.log(`Body: ${body}`)
     
-            let dailyPlan = JSON.parse(body)
+            let mealPlan = JSON.parse(body)
     
-            let message = `Your breakfast today is ${dailyPlan.meals[0].title}. Time to prepare: ${dailyPlan.meals[0].readyInMinutes} minutes. Your lunch today is ${dailyPlan.meals[1].title}. Time to prepare: ${dailyPlan.meals[1].readyInMinutes} minutes. Your dinner today is ${dailyPlan.meals[2].title}. Time to prepare: ${dailyPlan.meals[2].readyInMinutes} minutes.`
-            console.log(dailyPlan)
-            console.log(message)
+            if(mealPlan.meals == undefined){
+                res.render('index', {dailyCals:null, error: "Enter a valid number of calories."})
+            } else {
+
+                let mealPlanText = `Your breakfast today is ${mealPlan.meals[0].title}. Time to prepare: ${mealPlan.meals[0].readyInMinutes} minutes. Your lunch today is ${mealPlan.meals[1].title}. Time to prepare: ${mealPlan.meals[1].readyInMinutes} minutes. Your dinner today is ${mealPlan.meals[2].title}. Time to prepare: ${mealPlan.meals[2].readyInMinutes} minutes.`
+                console.log(mealPlan)
+                console.log(mealPlanText)
+
+                res.render('index', { mealPlan: mealPlanText, error:null})
+            }
         }
     
     
@@ -51,10 +61,10 @@ app.post('/', (req, res)=> {
 //     }else {
 //         console.log(`Body: ${body}`)
 
-//         let dailyPlan = JSON.parse(body)
+//         let mealPlan = JSON.parse(body)
 
-//         let message = `Your breakfast today is ${dailyPlan.meals[0].title}. Time to prepare: ${dailyPlan.meals[0].readyInMinutes} minutes. `
-//         console.log(dailyPlan)
+//         let message = `Your breakfast today is ${mealPlan.meals[0].title}. Time to prepare: ${mealPlan.meals[0].readyInMinutes} minutes. `
+//         console.log(mealPlan)
 //         console.log(message)
 //     }
 
@@ -65,7 +75,7 @@ app.post('/', (req, res)=> {
 
 
 
-const port = 5000
+const port = process.env.PORT
 app.listen(port, () => {
     console.log(`this server is running on ${port}`)
 })
