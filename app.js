77fb48eec074b5529d3ffd5a5c857078f654a7dd
Rehan Siraj <rsiraj@r2hstudent.org>
2022-02-18 00:31:07 -0500

    worked on backend

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const app = express()
 require('dotenv').config()
 const bodyParser = require('body-parser')
 
+
 const apiKey = process.env.APIKEY
 const apiKey2 = process.env.APIKEY2
-// let dailyCals = 2300
-// let diet = 'vegetarian'
-
-// let url = `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day&targetCalories=${dailyCals}&diet=${diet}`
-
-
 
 app.set('view engine', 'ejs')
 app.use (bodyParser.urlencoded({extended: true}));
-app.use (express.static('public'));
+app.use (express.static(__dirname + "/public"));
+
 
 app.get('/', (req, res)=> {
     res.render('index')
+})
+
+app.get('/meals', (req, res)=> {
+    res.render('mealsPage')
 })
 
 mealText = ""
@@ -28,8 +28,9 @@ nutrition = ""
 error = ""
 activityVariable = ""
 value = ""
+activity = ""
 
-app.post('/', (req, res)=> {
+app.post('/meals', (req, res)=> {
     let dailyCals = req.body.dailyCals
     let diet = req.body.diet
 
@@ -39,63 +40,44 @@ app.post('/', (req, res)=> {
         if(err){
             console.log(`Error: ${err}`)
 
-            res.render('index', {mealPlans:null, error: err})
+            res.render('mealsPage', {mealText:null, error: err})
         }else {
             // console.log(`Body: ${body}`)
     
             let mealPlan = JSON.parse(body)
     
             if(mealPlan.meals == undefined){
-                res.render('index', {mealPlans:null, error: "Enter a valid number of calories."})
+                res.render('mealsPage', {mealText:null, error: "Enter a valid number of calories."})
+
             } else {
-                let breakfastText = `Your breakfast today is ${mealPlan.meals[0].title}. Time to prepare: ${mealPlan.meals[0].readyInMinutes} minutes.`
-                let lunchText = `Your lunch today is ${mealPlan.meals[1].title}. Time to prepare: ${mealPlan.meals[1].readyInMinutes} minutes.`
-                let dinnerText = `Your dinner today is ${mealPlan.meals[2].title}. Time to prepare: ${mealPlan.meals[2].readyInMinutes} minutes.`
-                let breakfastImage = `https://spoonacular.com/recipeImages/${mealPlan.meals[0].id}-312x231.jpg`
-                let lunchImage = `https://spoonacular.com/recipeImages/${mealPlan.meals[1].id}-312x231.jpg`
-                let dinnerImage = `https://spoonacular.com/recipeImages/${mealPlan.meals[2].id}-312x231.jpg`
-                let breakfastURL = mealPlan.meals[0].sourceUrl
-                let lunchURL = mealPlan.meals[1].sourceUrl
-                let dinnerURL = mealPlan.meals[2].sourceUrl
-                let mealText = [breakfastText, lunchText, dinnerText]
-                let mealImages = [breakfastImage, lunchImage, dinnerImage]
-                let mealUrls = [breakfastURL, lunchURL, dinnerURL]
+                let mealText = [
+                breakfastText = `Your breakfast today is ${mealPlan.meals[0].title}. Servings: ${mealPlan.meals[0].servings}. Time to prepare: ${mealPlan.meals[0].readyInMinutes} minutes.`,
+                lunchText = `Your lunch today is ${mealPlan.meals[1].title}. Servings: ${mealPlan.meals[1].servings}. Time to prepare: ${mealPlan.meals[1].readyInMinutes} minutes.`,
+                dinnerText = `Your dinner today is ${mealPlan.meals[2].title}. Servings: ${mealPlan.meals[2].servings}. Time to prepare: ${mealPlan.meals[2].readyInMinutes} minutes.`
+                ]
+                let mealImages = [
+                (breakfastImage = `https://spoonacular.com/recipeImages/${mealPlan.meals[0].id}-312x231.jpg`),
+                lunchImage = `https://spoonacular.com/recipeImages/${mealPlan.meals[1].id}-312x231.jpg`,
+                dinnerImage = `https://spoonacular.com/recipeImages/${mealPlan.meals[2].id}-312x231.jpg`
+                ]
+                let mealUrls = [
+                breakfastURL = mealPlan.meals[0].sourceUrl,
+                lunchURL = mealPlan.meals[1].sourceUrl,
+                dinnerURL = mealPlan.meals[2].sourceUrl,
+                ]
                 let nutrition = `Your meals today add up to a total of ${mealPlan.nutrients.calories} calories. Your macros are Protein: ${mealPlan.nutrients.protein}g, Fat: ${mealPlan.nutrients.fat}g, Carbs: ${mealPlan.nutrients.carbohydrates}g.`
                 
-                res.render('index', { mealText, mealImages, mealUrls, nutrition,  error:null})
+                res.render('mealsPage', { mealText, mealImages, mealUrls, nutrition,  error:null})
             }
         }
-    
-    
     })
-
 })
 
-app.get('/api/:activity', (req, res)=> {
+app.get('/:activity', (req, res)=> {
     let userActivity = req.params.activity
     let url = `https://www.google.com/maps/embed/v1/search?q=${userActivity}%20near%20me&key=${apiKey2}`
-    res.render('mapPage', {url})
+    res.render('map', {url})
 })
-
-// request (url, function(err, res, body){
-//     if(err){
-//         console.log(`Error: ${err}`)
-//     }else {
-//         console.log(`Body: ${body}`)
-
-//         let mealPlan = JSON.parse(body)
-
-//         let message = `Your breakfast today is ${mealPlan.meals[0].title}. Time to prepare: ${mealPlan.meals[0].readyInMinutes} minutes. `
-//         console.log(mealPlan)
-//         console.log(message)
-//     }
-
-
-// })
-
-
-
-
 
 const port = process.env.PORT
 app.listen(port, () => {
