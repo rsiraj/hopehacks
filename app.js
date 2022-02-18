fd7d4bd57662abe27a82cd78c440f9092ece49d1
Rehan Siraj <rsiraj@r2hstudent.org>
2022-02-18 00:32:32 -0500

    im tired

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const apiKey2 = process.env.APIKEY2
 
 app.set('view engine', 'ejs')
 app.use (bodyParser.urlencoded({extended: true}));
-app.use (express.static(__dirname + "/public"));
+app.use (express.static("public"));
 
 
 app.get('/', (req, res)=> {
